34d81015a090e5819d1edfc49d313fadf2cbad52
Saheed Abiola Busari <integralsabiola@hotmail.com>
2015-02-20 19:02:35 +0000

    saheed

---
+++ EvoPaj.Base/Entities/Printer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EvoPaj.Base.Entities
+{
+    public class Printer
+    {
+        public virtual long ID {set;get;}
+    }
+}
